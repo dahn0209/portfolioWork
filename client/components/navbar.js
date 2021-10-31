d1017cfcb551eba44bddd2f664eb81a5f05b4347
dahn0209 <dahn0209@gmail.com>
2021-10-30 23:52:44 -0400

    edite navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 id="title">Restaurant Bar</h1>
+    <h1>Restaurant and Bar</h1>
     <nav>
       {isLoggedIn ? (
         <div>
